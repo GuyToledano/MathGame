ed99706b6365ed3346ed435dcb3cdcb5442b82e9
Guy Toledano <ferro.g.to@outlook.co.il>
2015-04-07 11:56:26 +0300

    math game header

--- EXE1/TheMathGame.h
+++ EXE1/TheMathGame.h
@@ -4,8 +4,23 @@
 #include <iostream>
 #include "ISpecificGame.h"
 #include "Player.h"
+
 #define LAST_LEVEL 20
 #define FIRST_LEVEL 0
+#define PLAYER_1 1
+#define PLAYER_2 2
+#define PLAYER_1_UP 'w'
+#define PLAYER_1_DOWN 'x'
+#define PLAYER_1_LEFT 'a'
+#define PLAYER_1_RIGHT 'd'
+#define PLAYER_2_UP 'i'
+#define PLAYER_2_DOWN 'm'
+#define PLAYER_2_LEFT 'j'
+#define PLAYER_2_RIGHT 'l'
+#define X_MAX 79
+#define X_MIN 0
+#define Y_MAX 23
+#define Y_MIN 3
 
 using namespace std;
 
